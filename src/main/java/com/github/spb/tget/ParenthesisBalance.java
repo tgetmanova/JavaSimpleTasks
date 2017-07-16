92bfa7c86199e3ddad198f8375515775f34241df
tgetmanova <tatyana.getmanova@gmail.com>
2017-07-16 23:55:35 +0300

    Draft (to be refactored!) for multi brackets balance verification

--- src/main/java/com/github/spb/tget/ParenthesisBalance.java
+++ src/main/java/com/github/spb/tget/ParenthesisBalance.java
@@ -20,4 +20,169 @@ public class ParenthesisBalance {
 
         return openingCount == closingCount;
     }
+
+    public static boolean isBalancedComplex(String text) {
+        String extractedBraces = extractBraces(text);
+        char[] chars = extractedBraces.toCharArray();
+        int openingParenthesisCount = 0, closingParenthesisCount = 0;
+        int openingBraceCount = 0, closingBraceCount = 0;
+        int openingBracketCount = 0, closingBracketCount = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            switch (chars[i]) {
+                case '(':
+                    openingParenthesisCount++;
+                    break;
+                case ')':
+                    closingParenthesisCount++;
+                    if ((closingBraceCount < openingBraceCount || closingBracketCount < openingBracketCount)
+                            && !consistentBefore(i, extractedBraces.substring(0, i + 1))) {
+                        return false;
+                    }
+                    break;
+                case '{':
+                    openingBraceCount++;
+                    break;
+                case '}':
+                    closingBraceCount++;
+                    if ((closingParenthesisCount < openingParenthesisCount || closingBracketCount < openingBracketCount)
+                            && !consistentBefore(i, extractedBraces.substring(0, i + 1))) {
+                        return false;
+                    }
+                    break;
+                case '[':
+                    openingBracketCount++;
+                    break;
+                case ']':
+                    closingBracketCount++;
+                    if ((closingBraceCount < openingBraceCount || closingParenthesisCount < openingParenthesisCount)
+                            && !consistentBefore(i, extractedBraces.substring(0, i + 1))) {
+                        return false;
+                    }
+                    break;
+                default:
+                    throw new IllegalStateException("Some unexpected char: " + chars[i]);
+            }
+
+            if (closingParenthesisCount > openingParenthesisCount
+                    || closingBraceCount > openingBraceCount
+                    || closingBracketCount > openingBracketCount) {
+                return false;
+            }
+        }
+
+        return closingParenthesisCount == openingParenthesisCount
+                && closingBraceCount == openingBraceCount
+                && closingBracketCount == openingBracketCount;
+    }
+
+    private static boolean consistentBefore(int currentCharIndex, String stringBefore) {
+        char[] chars = stringBefore.toCharArray();
+
+        int beginIndex = findCorrespondingOpening(chars, chars[currentCharIndex]);
+
+        switch (chars[currentCharIndex]) {
+            case ')':
+                return validateBracesCountWithin(stringBefore.substring(beginIndex, currentCharIndex + 1))
+                        && validateBracketsCountWithin(stringBefore.substring(beginIndex, currentCharIndex + 1));
+            case '}':
+                return validateParenthesisCountWithin(stringBefore.substring(beginIndex, currentCharIndex + 1))
+                        && validateBracketsCountWithin(stringBefore.substring(beginIndex, currentCharIndex + 1));
+            case ']':
+                return validateBracesCountWithin(stringBefore.substring(beginIndex, currentCharIndex + 1))
+                        && validateParenthesisCountWithin(stringBefore.substring(beginIndex, currentCharIndex + 1));
+            default:
+                throw new IllegalStateException("Some unexpected char: " + chars[currentCharIndex]);
+        }
+    }
+
+    private static int findCorrespondingOpening(char[] chars, char currentChar) {
+        char oppositeChar = 0;
+
+        switch (currentChar) {
+            case ')':
+                oppositeChar = '(';
+                break;
+            case '}':
+                oppositeChar = '{';
+                break;
+            case ']':
+                oppositeChar = '[';
+                break;
+            default:
+                throw new IllegalStateException("Some unexpected char: " + currentChar);
+        }
+        int competitors = 0;
+        for (int i = chars.length - 2; i >= 0; i--) {
+            if (chars[i] == currentChar) {
+                competitors++;
+            } else if (chars[i] == oppositeChar) {
+                if (competitors == 0) {
+                    return i;
+                } else {
+                    competitors--;
+                }
+            }
+        }
+
+        throw new IllegalStateException("No corresponding opposite character found");
+    }
+
+    private static boolean validateParenthesisCountWithin(String substring) {
+        char[] chars = substring.toCharArray();
+        int openingCount = 0, closingCount = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            switch (chars[i]) {
+                case '(':
+                    openingCount++;
+                    break;
+                case ')':
+                    closingCount++;
+                    break;
+            }
+        }
+
+        return closingCount == openingCount;
+    }
+
+    private static boolean validateBracesCountWithin(String substring) {
+        char[] chars = substring.toCharArray();
+        int openingCount = 0, closingCount = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            switch (chars[i]) {
+                case '{':
+                    openingCount++;
+                    break;
+                case '}':
+                    closingCount++;
+                    break;
+            }
+        }
+
+        return closingCount == openingCount;
+    }
+
+    private static boolean validateBracketsCountWithin(String substring) {
+        char[] chars = substring.toCharArray();
+        int openingCount = 0, closingCount = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            switch (chars[i]) {
+                case '[':
+                    openingCount++;
+                    break;
+                case ']':
+                    closingCount++;
+                    break;
+            }
+        }
+
+        return closingCount == openingCount;
+    }
+
+    private static String extractBraces(String text) {
+        return text.replaceAll("[^\\[\\](){}]", "");
+    }
 }
--- src/test/java/com/github/spb/tget/test/ParenthesisBalanceTest.java
+++ src/test/java/com/github/spb/tget/test/ParenthesisBalanceTest.java
@@ -8,7 +8,7 @@ import org.testng.annotations.Test;
 public class ParenthesisBalanceTest {
 
     @DataProvider
-    public static Object[][] getTestCasesData() {
+    public static Object[][] getTestCasesSingleType() {
         return new Object[][]{
                 {")", false},
                 {"(", false},
@@ -26,9 +26,52 @@ public class ParenthesisBalanceTest {
         };
     }
 
-    @Test(dataProvider = "getTestCasesData")
+    @Test(dataProvider = "getTestCasesSingleType")
     public void parenthesisBalance_isBalanced_ShouldReturnTrue_forBalancedTexts(String textToValidate, boolean expectedResult) {
         boolean actualResult = ParenthesisBalance.isBalanced(textToValidate);
         Assert.assertEquals(actualResult, expectedResult, "Incorrect isBalanced result");
+    }
+
+    @DataProvider
+    public static Object[][] getTestCasesMultiTypes() {
+        return new Object[][]{
+                {")", false},
+                {"(", false},
+                {"(a)", true},
+                {")der(", false},
+                {"d)ss(s(ss", false},
+                {"dd)(dsdds", false},
+                {"(s)ss)sd", false},
+                {"()()()()", true},
+                {"(((())))", true},
+                {"(d(sds(dsdd))sds)aaa", true},
+                {"(((()())))", true},
+                {"ss(((s()ss())ss))", true},
+                {"(s(s()(ss())ss))", true},
+                {"()[]", true},
+                {"([])", true},
+                {"(dd[dsfd]sds(sd)lld{4})", true},
+                {"(dd[ds{234}fd]sds(sd)lld{4})", true},
+                {"(dd[ds{234}f(d)d]sds(sd)lld{4})", true},
+                {"(dd[d[]d]sds)", true},
+                {"sss(dd[d[](){s(s)}d]sds)ss", true},
+                {"vvv{}vv{v{v{}vv{vv}vvvv}v{s}v}vv", true},
+                {"vv{[vvvv{[vvvvvv]}vvvvvvv]}vvvv", true},
+                {"ddddd{d()ddd{d[ddd()ddddddd]d}d}dddd", true},
+                {"[[]]", true},
+                {"([", false},
+                {"([)]", false},
+                {"1(hhs[s)lll)", false},
+                {"sss(dd[d[](){ss)}d]sds)ss", false},
+                {"vvv{}vv{v{v{}vv{vv}vvvv}v{s}vvv", false},
+                {"vv{[vvvv{vvvvvv]}vvvvvvv]}vvvv", false},
+                {"ddddd{d()ddd{d[ddd(ddddddd]d}d}dddd", false},
+        };
+    }
+
+    @Test(dataProvider = "getTestCasesMultiTypes")
+    public void parenthesisBalance_isBalancedComplex_ShouldReturnTrue_forBalancedTexts(String textToValidate, boolean expectedResult) {
+        boolean actualResult = ParenthesisBalance.isBalancedComplex(textToValidate);
+        Assert.assertEquals(actualResult, expectedResult, "Incorrect isBalancedComplex result");
     }
 }
